84dc01ef397f33a8b7150019ed7ec2418f51a487
alvin <aocasiojr@my.nl.edu>
2023-10-31 14:42:19 -0500

    mod 6

--- my_module.py
+++ my_module.py
@@ -1,10 +1,7 @@
 def greet(name):
     print(f"hello, {name}!")
-
 class person:
-    def __init__(self, name):
-        self.name= name
-
-class person:
+    def __init__(self,name):
+            self.name = name
     def say_hello(self):
-        print(f"{self.name}say hello!")
+            print(f"{self.name} say hello!")
